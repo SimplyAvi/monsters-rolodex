afa3102c5407a33191287b5fa497650f886c9b9b
SimplyAvi <a.totaram@gmail.com>
2020-03-23 20:40:22 -0400

    added a little styling so that all the names were displayed within each row

--- src/App.js
+++ src/App.js
@@ -18,14 +18,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        < CardList name="avi" />
+        < CardList name="avi" >
         {
-          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
-        }
+          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)}
+        </ CardList> 
       </div>
-      )
-    }
-
+      )}
 }
 
 export default App;
