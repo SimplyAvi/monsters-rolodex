bf95f7eb3172bf4ed21c50c8f16e24dddfd0a8b4
SimplyAvi <a.totaram@gmail.com>
2020-03-30 19:51:56 -0400

    Added tit for the page

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         < SearchBox placeholder="search monster" handleChage={ e => this.setState({ searchField: e.target.value}) }/ >
         < CardList  monsters ={filteredMonster}/>
 
