f9dbc584bc183df11f15eb47be630019b4cba96e
SimplyAvi <a.totaram@gmail.com>
2020-03-30 20:03:58 -0400

    Updates

---
+++ precache-manifest.1bd5e40367a31f82a4056cc153b86cef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab58686a9c5c4f33e3b4ae69a20a7843",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a5fbf0775fb0e3a8efc4",
+    "url": "/monsters-rolodex/static/css/main.75665b0b.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5fbf0775fb0e3a8efc4",
+    "url": "/monsters-rolodex/static/js/main.2dc871a1.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
